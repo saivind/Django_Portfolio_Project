62b6ba632746a229dcc56ad19a01db70e2777af3
saivind <saivindhyachebrolu@gmail.com>
2023-07-10 11:09:04 +0200

    changed views.py and urls.py

--- django_project/portfolio/urls.py
+++ django_project/portfolio/urls.py
@@ -1,11 +1,9 @@
 from django.conf import settings
 from django.urls import path
-from . import views
+from portfolio.views import api_np
 
 app_name='portfolio'
 
 urlpatterns = [
-#NP_API
-                              path('results',views.api_np, name='results'),
-                              path('info_NP',views.api_np, name='info_NP'),
+                              path('info', api_np, name='info_NP'),
 ]
